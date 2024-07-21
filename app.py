659c84d0eea6277290a0f02db88759622b6e2874
raidmt <raidmt4322@gmail.com>
2024-07-21 02:22:41 +0000

    added dynamic data  and api route

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'hair cut',
+    'location': 'Bengaluru, India',
+    'price': 'Rs. 100'
+  },
+  {
+    'id': 2,
+    'title': 'spa',
+    'location': 'Bengaluru, India',
+    'price': 'Rs. 200'
+  },
+{
+  'id': 3,
+  'title': 'shavingt',
+  'location': 'Bengaluru, India',
+  'price': 'Rs. 80'
+}
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('Home.html')
+    return render_template('Home.html',jobs=JOBS,company_name='STYLESENSE')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
